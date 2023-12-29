d2d944cfa60f15c430b3ff6a06123822fd1f2888
Ecpopov <ecpopov@yandex.ru>
2023-12-29 12:31:06 +0300

    add poetry

---
+++ core/config.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from pydantic import BaseModel
+from pydantic_settings import BaseSettings
+
+BASE_DIR = Path(__file__).parent.parent
+
+DB_PATH = BASE_DIR / "db.sqlite3"
+
+
+class DbSettings(BaseModel):
+    url: str = "postgresql+asyncpg://postgres:123@localhost/fast_db_d"
+    # echo: bool = False
+    echo: bool = True
+
+
+class AuthJWT(BaseModel):
+    private_key_path: Path = BASE_DIR / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "certs" / "jwt-public.pem"
+    algorithm: str = "RS256"
+    access_token_expire_minutes: int = 15
+    # access_token_expire_minutes: int = 3
+
+
+class Settings(BaseSettings):
+    api_v1_prefix: str = "/api/v1"
+
+    db: DbSettings = DbSettings()
+
+    auth_jwt: AuthJWT = AuthJWT()
+
+    # db_echo: bool = True
+
+
+settings = Settings()
